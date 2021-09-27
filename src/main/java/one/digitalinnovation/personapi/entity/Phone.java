1283661ccd7957b4cb110a361bfa9e5583756744
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-09-27 14:36:13 -0300

    - entities, phone type, person repository created and added h2 test db

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private PhoneType phoneType;
+
+    @Column(nullable = false)
+    private String number;
+}
---
+++ src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.personapi.repository;
+
+public interface PersonRepository {
+
+
+
+}
